5fe641d7b4a94e0d7fd66c0683315c999caf560e
matheuslima23 <ml3666763@gmail.com>
2022-10-26 10:14:34 -0300

    adicionando fuções depesquisa

--- calendario.js
+++ calendario.js
@@ -1,12 +1,14 @@
 function mostraData() {
-    let data = document.getElementById("data").value;
+    let data = document.getElementById("date").value;
     data=new Date(data).toLocaleDateString("pt-BR")
+    console.log(data)
     let blocoEvento = document.getElementById("eventos-teatro");
     let eventos = blocoEvento.getElementsByClassName("evento");
     for (let i = 0; i < eventos.length; i++) {
-        if (eventos[i].getElementByClassName("data").value == data) {
-            console.log("evevntos para o dia " + data);
-            console.log(eventos[i].getElementByClassName("titulo").value);
-        }
+      console.log(eventos[i])
+        // if (eventos[i].getElementByClassName("data").value == data) {
+        //     console.log("evevntos para o dia " + data);
+        //     console.log(eventos[i].getElementByClassName("titulo").value);
+        // }
     }
 }
