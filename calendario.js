1e3dbf4c9867a19ab4c236409734c599dd70ae9d
matheuslima23 <ml3666763@gmail.com>
2022-10-26 09:32:14 -0300

    função para verificar as datas dos eventos

--- calendario.js
+++ calendario.js
@@ -1,10 +1,12 @@
-function mostraData(){
-    let data=document.getElementById("data").value
-   let dataDoevento=();
-
-  if (data==dataDoevento){
-console.log("evevntos para o dia "+data+"")
-  }else{ console.log("não haverá eventos hj :(")
-  }
-    //console.log(data)
+function mostraData() {
+    let data = document.getElementById("data").value;
+    data=new Date(data).toLocaleDateString("pt-BR")
+    let blocoEvento = document.getElementById("eventos-teatro");
+    let eventos = blocoEvento.getElementsByClassName("evento");
+    for (let i = 0; i < eventos.length; i++) {
+        if (eventos[i].getElementByClassName("data").value == data) {
+            console.log("evevntos para o dia " + data);
+            console.log(eventos[i].getElementByClassName("titulo").value);
+        }
+    }
 }
